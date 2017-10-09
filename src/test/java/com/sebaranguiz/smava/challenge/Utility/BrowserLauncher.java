14e64822f41de08db6a43da0c9777aad4bc1386c
sebaranguiz <32102635+sebaranguiz@users.noreply.github.com>
2017-10-09 11:40:01 +0200

    final version

--- src/test/java/com/sebaranguiz/smava/challenge/Utility/BrowserLauncher.java
+++ src/test/java/com/sebaranguiz/smava/challenge/Utility/BrowserLauncher.java
@@ -13,7 +13,7 @@ public class BrowserLauncher {
 		/* The geckodriver for Firefox Linux version (64 bits) is included on the path: 
 		 * src/test/resources/drivers/linux64. In case of using a different OS please download 
 		 * the suitable geckodriver version from the Selenium's Website and update the path below */
-		System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/linux64/geckodriver.exe"); 
+		System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/linux64/geckodriver"); 
 		driver = new FirefoxDriver();
 		driver.get(url);
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
